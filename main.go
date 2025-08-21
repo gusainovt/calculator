dafc9876882323137afb23ea154e9acdca78ea2e
gusainovt <timurka24478@gmail.com>
2025-08-21 13:40:42 +0300

    add post endpoint

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -15,7 +16,7 @@ type Calculation struct {
 	Result     string `json:"result"`
 }
 
-type CalculationResult struct {
+type CalculationRequest struct {
 	Expression string `json:"expression"`
 }
 
@@ -37,12 +38,34 @@ func getCalculations(c echo.Context) error {
 	return c.JSON(http.StatusOK, calculations)
 }
 
+func postCalculations(c echo.Context) error {
+	var req CalculationRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	result, err := calculateExpression(req.Expression)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+	calc := Calculation{
+		ID:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     result,
+	}
+	calculations = append(calculations, calc)
+	return c.JSON(http.StatusCreated, calc)
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
 	e.GET("/calculations", getCalculations)
+	e.POST("/calculations", postCalculations)
 
 	e.Start("localhost:8080")
 }
