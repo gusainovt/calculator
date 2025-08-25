34885b3c9731ad878da78870fe40e789589772a3
gusainovt <timurka24478@gmail.com>
2025-08-25 10:38:15 +0300

    refactor: init db

---
+++ internal/db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initDB() {
+	dsn := "host=localhost user=postgres password=1234 dbname=calculator_db port=5432 sslmode=disable"
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&Calculation{}); err != nil {
+		log.Fatalf("Could not migrate table: %v", err)
+	}
+}
