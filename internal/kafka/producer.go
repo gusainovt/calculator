0be03a092131b6c17c31358af4470d6155f5b3de
gusainovt <timurka24478@gmail.com>
2025-09-03 15:16:02 +0300

    add kafka producer

---
+++ internal/kafka/producer.go
@@ -0,0 +1,61 @@
+package kafka
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+const (
+	flushTimeout = 500
+)
+
+var errUnknowType = errors.New("unknow event type")
+
+type Producer struct {
+	producer *kafka.Producer
+}
+
+func NewProducer(address []string) (*Producer, error) {
+	config := &kafka.ConfigMap{
+		"bootstrap.servers": strings.Join(address, ","),
+	}
+	p, err := kafka.NewProducer(config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating the Kafka producer: %s", err)
+	}
+
+	return &Producer{producer: p}, nil
+}
+
+func (p *Producer) Produce(message, topic string) error {
+	kafkaMsg := &kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: []byte(message),
+		Key:   nil,
+	}
+	kafkaChan := make(chan kafka.Event)
+	if err := p.producer.Produce(kafkaMsg, kafkaChan); err != nil {
+		return fmt.Errorf("error sending message to Kafka: %s", err)
+	}
+	e := <-kafkaChan
+
+	switch ev := e.(type) {
+	case *kafka.Message:
+		return nil
+	case kafka.Error:
+		return ev
+	default:
+		return errUnknowType
+	}
+}
+
+func (p *Producer) Close() {
+	p.producer.Flush(flushTimeout)
+	p.producer.Close()
+}
